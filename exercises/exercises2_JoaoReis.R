87260edb8e77f995415597400f2296b81ba1b494
Reis-Joao <joaobernardoalmeidareis@gmail.com>
2023-02-22 18:43:53 +0100

    changes on the second exercise

--- exercises/exercises2_JoaoReis.R
+++ exercises/exercises2_JoaoReis.R
@@ -4,53 +4,59 @@
 # search the help (via `??`) for specific types of functions, or read
 # about a library; post on Slack or search online.
 
+library(nycflights13)
+library(dplyr)
+library(stringr)
+library(tidyverse)
+
 # Exercise 1: Get all the columns from the NYC 2013 flights data 
 # that contain data about a time (of arrival, departure, etc) - but
 # not about duration (e.g. flight duration). I.e. it is an event that
 # happened at a specific time. Keep year, month, day too.
-library(nycflights13)
-View(flights)
 # Note: time_hour also counts, but air_time does not - it is about the 
 # duration spent in the air, so it does not happen at a specific time.
+
 flights_time <- flights %>%
   select(year, month, day, dep_time, sched_dep_time, arr_time, sched_arr_time, time_hour)
 
-# Should we include the columns hour and minute?
+flights_time
 
-View(flights_time)
 
 # Exercise 2: Get all the columns that contain text (i.e. character <chr>
 # type) and where the contents of the columns are at most 3 characters 
 # long. (NA data counts as 0-length.) 
 # Look up how to get the length of strings in library stringr.
-library(dplyr)
-library(stringr)
 
 flights_text <- flights %>%
-  select_if(is.character) %>%   # select only character type columns
-  select_if(~sum(str_length(na_if(.,"")) <= 3) > 0)  # select columns with length <= 3
+  select_if(is.character) %>% 
+  select_if(~all(str_length(na.omit(.)) <= 3)) 
+  # I am intentionally keeping columns that might have some NA observations
+  # Since it makes no sense to exclude all the column just because some observations might be unknown.
 
-View(flights_text)
+flights_text
+
 
 # Exercise 3: arrange the starwars characters from tallest to smallest, 
 # with NA's at the top. 
-library(tidyverse)
 
 starwars_by_height <- starwars %>% 
   arrange(desc(is.na(height)), desc(height))
 
-View(starwars_by_height)
+starwars_by_height
+
 
 # Exercise 4: arrange the starwars characters by species, then from 
 # tallest to smallest WITHIN THEIR SPECIES! The NA's can be at the 
 # bottom. Reorder the columns so that the name comes first, then the
 # height, then the species, then the rest.
+
 starwars_by_height_by_species <- starwars %>% 
   group_by(species) %>% 
   arrange(species, desc(height), na.last = TRUE) %>% 
   select(name, height, species, everything())
 
-View(starwars_by_height_by_species)
+starwars_by_height_by_species
+
 
 # Exercise 5: the preceding exercises asked you to compute the given
 # dataframe for a specific known dataset. Suppose that we give you a
@@ -73,20 +79,51 @@ View(starwars_by_height_by_species)
 # You should test that your code works by trying out some subsets,
 # both column- and row-wise.
 
-# FIXME: comment out and define function
-# flights_time_function <- ...
+flights_time_function <- function(data){
+  data %>%
+    select(any_of(c("year", "month", "day", "dep_time", "sched_dep_time", "arr_time", "sched_arr_time", "time_hour"))) 
+    #https://stackoverflow.com/questions/43786883/how-do-i-select-columns-that-may-or-may-not-exist
+} 
+
+flights_subset1 <- select(flights, year, month, day, time_hour, carrier)
+flights_subset2 <- select(flights, year, month, day, dep_time, origin, hour, minute)
+flights_time_function(flights_subset1)
+flights_time_function(flights_subset2)
+
 
 # This one is probably hard to do without `where` and understanding
 # functions as arguments. Search for another solution.
 
-# FIXME: comment out and define function
-# flights_text_function <- ...
+flights_text_function <- function(data){
+  data %>%
+    select_if(is.character) %>%
+    mutate_all(~na_if(., "")) %>%
+    select_if(~all(str_length(na.omit(.)) <= 3))
+}
+    # In here I am also transforming empty strings into NAs, in case Professor adds some empty rows.
+    # Like that I keep a column that has every non-NA values up to 3 characters, even when it has NA/empty strings
+    # This avoids the code to return an empty variable if, for example, someone adds an empty row by mistake
+    # https://sparkbyexamples.com/r-programming/replace-empty-string-with-na-in-r-dataframe/#:~:text=R%20%E2%80%93%20Replace%20Empty%20String%20with%20NA%201,Replace%20on%20All%20Character%20columns%20...%20Mais%20itens
+flights_subset1 <- select(flights, year, month, day, time_hour, carrier, dest, origin)
+flights_subset2 <- select(flights, year, time_hour, origin)
+flights_text_function(flights_subset1)
+flights_text_function(flights_subset2)
+
 
 # Assume that species and height are present so you can arrange by.
 # Do not assume that name is present, but put it first if it is.
 
-# FIXME: comment out and define function
-# starwars_by_height_by_species_function <- ...
+starwars_by_height_by_species_function <- function(data) {
+  data %>%
+    group_by(species) %>%
+    arrange(species, desc(height), na.last = TRUE) %>%
+    select(any_of(c("name", "height", "species")), everything()) 
+}
+
+starwars_subset1 <- select(starwars, height, species, films, skin_color)
+starwars_subset2 <- select(starwars, name, height, species, sex, vehicles)
+starwars_by_height_by_species_function(starwars_subset1)
+starwars_by_height_by_species_function(starwars_subset2)
 
 
 # Exercise 6: Using the flights data, for each carrier, find the flight 
@@ -94,19 +131,32 @@ View(starwars_by_height_by_species)
 # Put these 2 numbers into a column of their own, called 'best' (shortest 
 # delay) and 'worst' (longest delay).
 # Keep the columns carrier, best, and worst in that order.
+
 flights_carrier_best_worst <- flights %>%
   group_by(carrier) %>%
   summarize(best = min(dep_delay, na.rm = TRUE), worst = max(dep_delay, na.rm = TRUE)) %>%
   select(carrier, best, worst)
 
-View(flights_carrier_best_worst)
+flights_carrier_best_worst
 
-# Exercise 7: Find the days such that the most delayed flight on that
-# day is more delayed than the most delayed flight on the previous day.
+
+# Exercise 7: Find the days such that the most delayed flight (by departure)
+# on that day of the year is more delayed than the most delayed flight on the 
+# previous day. Keep year, month, day, and dep_delay columns.
+
 flights_days_getting_worse <- flights %>%
   group_by(year, month, day) %>%
-  summarize(max_arr_delay = max(arr_delay, na.rm = TRUE)) %>%
-  mutate(prev_day_max_delay = lag(max_arr_delay)) %>%
-  filter(max_arr_delay > prev_day_max_delay)
+  mutate(max_dep_delay = max(dep_delay, na.rm = TRUE)) %>%
+  ungroup() %>%
+  group_by(year, month) %>%
+  mutate(prev_day_max_delay = lag(max_dep_delay)) %>% 
+  #https://stackoverflow.com/questions/14689424/use-a-value-from-the-previous-row-in-an-r-data-table-calculation
+  ungroup() %>% 
+  filter(max_dep_delay > prev_day_max_delay) %>% 
+  select(year,month,day,dep_delay)
 
-View(flights_days_getting_worse)
+flights_days_getting_worse
+
+View(select(flights,month,day,dep_delay))
+
+
